a2f2b04482bf4764276752583c5968195ae358ff
Nikita Pogodaev <nik180101@gmail.com>
2022-03-27 02:47:45 +0700

    load more nodes data

--- src/main/java/ru/fit/nsu/np/xml/jaxb/OsmListJaxbProcessor.java
+++ src/main/java/ru/fit/nsu/np/xml/jaxb/OsmListJaxbProcessor.java
@@ -16,7 +16,7 @@ public abstract class OsmListJaxbProcessor<T extends PersistentEntity, Xml> exte
     @Override
     protected Consumer<List<T>> getResultConsumer() {
         return (list) -> {
-            list = list.subList(0, Math.min(list.size(), 10_000));
+            list = list.subList(0, Math.min(list.size(), 100_000));
             list.forEach(e -> e.setId(null));
             getService().save(list);
         };
