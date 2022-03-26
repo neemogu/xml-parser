a4ecd0e8467776c76af093896320ea3d4ac4e2b1
Nikita Pogodaev <nik180101@gmail.com>
2022-03-27 02:44:15 +0700

    nodes in radius order by

--- src/main/java/ru/fit/nsu/np/openmap/repository/NodeRepository.java
+++ src/main/java/ru/fit/nsu/np/openmap/repository/NodeRepository.java
@@ -9,7 +9,8 @@ import java.util.List;
 
 @Repository
 public interface NodeRepository extends EntityRepository<NodeEntity> {
-    @Query(value = "SELECT * FROM node WHERE earth_distance(ll_to_earth(node.lat, node.lon), ll_to_earth(:lat, :lon)) < :radius",
+    @Query(value = "SELECT * FROM node WHERE earth_distance(ll_to_earth(node.lat, node.lon), ll_to_earth(:lat, :lon)) < :radius " +
+            "ORDER BY earth_distance(ll_to_earth(node.lat, node.lon), ll_to_earth(:lat, :lon))",
             nativeQuery = true)
     List<NodeEntity> getNodesInRadius(@Param("lat") double lat, @Param("lon") double lon, @Param("radius") double radius);
 }
