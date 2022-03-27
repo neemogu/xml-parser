de8411d9ce074bc842daeda21c6f368033ba8f2b
Nikita Pogodaev <nik180101@gmail.com>
2022-03-27 14:14:55 +0700

    use jpql

--- src/main/java/ru/fit/nsu/np/openmap/repository/NodeRepository.java
+++ src/main/java/ru/fit/nsu/np/openmap/repository/NodeRepository.java
@@ -9,8 +9,8 @@ import java.util.List;
 
 @Repository
 public interface NodeRepository extends EntityRepository<NodeEntity> {
-    @Query(value = "SELECT * FROM node WHERE earth_distance(ll_to_earth(node.lat, node.lon), ll_to_earth(:lat, :lon)) < :radius " +
-            "ORDER BY earth_distance(ll_to_earth(node.lat, node.lon), ll_to_earth(:lat, :lon))",
-            nativeQuery = true)
+    @Query(value = "SELECT n FROM NodeEntity n " +
+            "WHERE FUNCTION('earth_distance', FUNCTION('ll_to_earth', n.lat, n.lon), FUNCTION('ll_to_earth', :lat, :lon)) < :radius " +
+            "ORDER BY FUNCTION('earth_distance', FUNCTION('ll_to_earth', n.lat, n.lon), FUNCTION('ll_to_earth', :lat, :lon))")
     List<NodeEntity> getNodesInRadius(@Param("lat") double lat, @Param("lon") double lon, @Param("radius") double radius);
 }
